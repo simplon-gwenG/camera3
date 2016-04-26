62f6b7b08f688bcd800f92673f0dd0373edc80c9
gwenaelle guezou <gwenaelle guezou>
2016-04-26 21:58:56 +0200

    avec cmmentaires

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -58,16 +58,16 @@ angular.module('starter')
             photo[j].ngSrc = $scope.images[j];//selection de la source (tableau d'images)
             var positionX = j*260;// variable qui crée un décalage de position d'image à chaque tour de tableau
             var positionY = 0;
-            context.save();
-            context.translate(200, 10)
-            context.rotate((Math.PI / 180) * 90)
-            context.strokeStyle = "#ffffff";
-            context.lineWidth = 10;
+            context.save();// sauvegarde du conevas initial
+            context.translate(200, 10)// déplacement de l'image
+            context.rotate((Math.PI / 180) * 90)// rotation de l'image
+            context.strokeStyle = "#ffffff";// cadre blanc autour de l'image
+            context.lineWidth = 10;// épaisseur du cadre
             //context.strokeRect(0,0,context.canvas.width, context.canvas.height);
             context.drawImage(photo[j],positionX,positionY,250,150);// dessine une image à chaque tour de boucle du tableau photo[j] avec les positions définies
     //dessine l'image  de l'index j
-    context.strokeRect(positionX,positionY,250, 150);
-            context.restore();
+    context.strokeRect(positionX,positionY,250, 150);// positionnment du rectangle autour de l'image
+            context.restore();// retour au context d'origine
 
             }
 
